e32f34ea978e5bde401e10979a95fb671d854e2c
Felipe Alves Tavares <felipealvests@hotmail.com>
2024-03-15 16:22:20 -0300

    Implemetei os controles para emoção, post e reação

--- Program.cs
+++ Program.cs
@@ -5,10 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DbEchoEmotional> (c => c.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 //app.MapGet("/", () => "Hello World!");
 
--- obj/Debug/net8.0/EchoEmotionalServer.AssemblyInfo.cs
+++ obj/Debug/net8.0/EchoEmotionalServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EchoEmotionalServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d7a467937bd960c8cfe5eaeaddac7ea09a6080b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+242d14482931b6548584175c51ae9fe9c8512f25")]
 [assembly: System.Reflection.AssemblyProductAttribute("EchoEmotionalServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EchoEmotionalServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
